35769207f51056d4f0fb8347617446b9d3b5e069
Ga1156 <gauravpardeshi068@gmail.com>
2023-05-04 03:57:16 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Component/Navbar';
+import AllRoutes from './Component/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
